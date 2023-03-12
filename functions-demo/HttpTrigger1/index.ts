91bdfd75bc8e4cfe3e0dacaec109d6bb26598ee1
Meir Shahar <meir.shahar@gmail.com>
2023-03-12 16:50:01 +0200

    Poc/change-http-content (#4)
    * updated content
    
    * more content

--- functions-demo/HttpTrigger1/index.ts
+++ functions-demo/HttpTrigger1/index.ts
@@ -7,10 +7,10 @@ const httpTrigger: AzureFunction = async function (
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
-    ? "Hello-yyy, " +
+    ? "Hello-xxx-zzz-yyy, " +
       name +
       ". This HTTP triggered function executed successfully."
-    : "yyy This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    : "XXX YYYYY ZZZZZ This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
   context.res = {
     // status: 200, /* Defaults to 200 */
